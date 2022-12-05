f8897932a5ff0dad41171ecf11ee1c486c6d1fe0
Abdulrahman-YE <abdulrahman1ye@gmail.com>
2022-12-06 00:19:14 +0300

    Change the structure of picoCTF folders

--- picoctf/2-mode-26/mode-26.py
+++ picoctf/2-mode-26/mode-26.py
@@ -1,4 +1,7 @@
+#!/bin/python3
+import sys
 text = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_hyLicInt}"
+
 lower_case = [97, 122]
 upper_case = [65, 90]
 def decipher(cipher : str) -> str:
@@ -6,16 +9,16 @@ def decipher(cipher : str) -> str:
     for char in cipher:
         ascii_value = ord(char)
         if  ascii_value >= lower_case[0] and ascii_value <= lower_case[1]:
-            d =  ascii_value + 12
-            if d >= lower_case[1]:
-                pad =  lower_case[0] + (d - lower_case[1])
+            distence =  ascii_value + 12
+            if distence >= lower_case[1]:
+                pad =  lower_case[0] + (distence - lower_case[1])
                 de_char = chr(pad)
             else:
                de_char = chr(ascii_value + 13)
         elif ascii_value >= upper_case[0] and ascii_value <= upper_case[1]:
-            d =  ascii_value + 12
-            if d >= upper_case[1]:
-                pad =  upper_case[0] + (d -  upper_case[1])
+            distence =  ascii_value + 12
+            if distence >= upper_case[1]:
+                pad =  upper_case[0] + (distence -  upper_case[1])
                 de_char = chr(pad)
             else : 
                 de_char  = chr(ascii_value + 13)
@@ -26,5 +29,9 @@ def decipher(cipher : str) -> str:
     return deciphered 
             
 if __name__ == "__main__":
-    print(decipher(text))
+    if len(sys.argv) < 2:
+        print(f'Usage : {sys.argv[0]} cipherred_text')
+        exit(1)
+    else:
+        print(decipher(sys.argv[1]))
     
